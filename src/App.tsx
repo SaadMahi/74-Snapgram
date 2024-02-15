ab7a73db6c7c2e91a92d1b0640a6bb5e7e74be1b
SaadMahi <saadmahimkar22@gmail.com>
2024-02-15 15:21:58 +0530

    🚀 feat: tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import "./globals.css";
-import { Route, Routes } from "react-router-dom";
-import AuthLayout from "./_auth/AuthLayout";
-import SigninForm from "./_auth/forms/SigninForm";
-import SignupForm from "./_auth/forms/SignupForm";
-import RootLayout from "./_root/RootLayout";
-import { Home } from "./_root/pages";
+import './globals.css';
+import { Route, Routes } from 'react-router-dom';
+import AuthLayout from './_auth/AuthLayout';
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import RootLayout from './_root/RootLayout';
+import { Home } from './_root/pages';
+import { Toaster } from '@/components/ui/toaster';
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
           <Route index element={<Home />} />
         </Route>
       </Routes>
+      <Toaster />
     </main>
   );
 };
